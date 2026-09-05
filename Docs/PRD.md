4ee84c0da17a5d1072965034e9df7eb59a4a9904
xdaamar <damarbudiprasetyo714@gmail.com>
2026-09-05 19:53:38 +0700

    Initial commit

---
+++ Docs/PRD.md
@@ -0,0 +1,4 @@
+# Product Requirement Document (PRD) - Platinum Cleaner 
+ 
+## 1. Overview 
+Aplikasi pembersih cache otomatis berbasis Native Android (Kotlin). 
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+pluginManagement { 
+    repositories { google(); mavenCentral(); gradlePluginPortal() } 
+} 
+rootProject.name = "PlatinumCleaner" 
+include(":app") 
